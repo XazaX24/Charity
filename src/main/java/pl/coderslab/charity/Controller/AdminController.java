f56b83fee5d33c9021eb8a685706c8bb478f41b9
Wojciech Gabara <wojtek_24@tlen.pl>
2024-07-25 12:53:27 +0200

    Add admin main page and dashboard template

---
+++ src/main/java/pl/coderslab/charity/Controller/AdminController.java
@@ -0,0 +1,17 @@
+package pl.coderslab.charity.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String admin(Model model) {
+
+        model.addAttribute("currentPage", "home");
+        return "admin";
+    }
+
+}
